e1f8c8d3e6e03f60ca629da8893fd48d5c733bd9
Julien Martin <balteo@gmail.com>
2018-09-30 16:37:38 +0200

    Initial commit for the Kafka Consumer App

---
+++ src/main/java/tests/kafka/consumer/KafkaConsumerApplication.java
@@ -0,0 +1,23 @@
+package tests.kafka.consumer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
+
+@Slf4j
+@SpringBootApplication
+public class KafkaConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaConsumerApplication.class, args);
+    }
+
+    @KafkaListener(topics = "myTopic")
+    public void listen(ConsumerRecord<?, ?> cr) throws Exception {
+        log.info("listening");
+        log.info(cr.toString());
+    }
+
+}
